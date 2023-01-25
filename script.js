69d30bdc3266403bbd2e313e66d76fce39a4bac3
antSur123 <113517280+antSur123@users.noreply.github.com>
2023-01-25 21:36:24 +0100

    Update 1
    Base funktionality done. Roughly, but done. Make this bad boy look prettier.

--- script.js
+++ script.js
@@ -3,101 +3,51 @@
  * https://www.youtube.com/watch?v=yq2au9EfeRQ&list=PLpPnRKq7eNW3We9VdCfx9fprhqXHwTPXL&index=3
  */
 
-
 // canvas size setup
 const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // draw on canvas
-const c = canvas.getContext('2d'); // draw a 2d shape
+const c = canvas.getContext('2d');
+const RADIUS = 35;
 
-/**
-// draw rect
-c.fillStyle = "rgba(225, 0, 0, 0.1)";
-c.fillRect(100, 100, 100, 100);
-
-// draw line
-c.beginPath(); // start drawing something from here
-c.moveTo(50, 300); // tp to
-c.lineTo(300,100); // draw to
-c.lineTo(300,400);
-c.strokeStyle = "blue";
-c.stroke(); // brush down
-
-// draw arc/circle
-for (let i = 0; i < 5; i++){
-    let x = Math.random() * canvas.width;
-    let y = Math.random() * canvas.height;
-    let r = Math.round(Math.random() * 225);
-    let g = Math.round(Math.random() * 225);
-    let b = Math.round(Math.random() * 225);
-    let color = `rgb(${r},${g},${b})`;
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = color;
-    c.stroke();
-}
-
-*/
-
-const DISTANCE_PX = 100;
-const MAX_RADIUS = 40;
-
-let clickArray = [];
-let avoidArray = [];
-
-
-let mouse = {
-    x: undefined,
-    y: undefined
-}
-
+let circleArray = [];
 
 let colorArray = [
-    "#003840",
-    "#005A5B",
-    "#007369",
-    "#008C72",
-    "#02A676"
+    "#3caea3",
+    "#f4c095",
+    "#ed553b",
+    "#f6d55c"
 ]
 
-window.addEventListener('mousemove', (event) => {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    avoidArray[0] = mouse;
-});
-
 window.addEventListener('mousedown', (event) => {
-    new Click(event.x, event.y)
-    console.log("clickArray", clickArray);
-    console.log("avoidArray", avoidArray);
+    getDistance(event.x, event.y);
+    
 });
 
-window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+function getDistance(mouseX, mouseY) {
+    let x = Number(mouseX);
+    let y = Number(mouseY);
 
-    init();
-});
-
-function Click(x, y) {
-    this.x = x;
-    this.y = y;
-    clickArray.push(this);
-    avoidArray.push(this);
-
+    let distance = Math.sqrt(Math.abs(Math.pow(x-circleArray[0].x,2)-Math.pow(y-circleArray[0].y,2)));
+    if (distance <= 38) {
+        console.log("Found!");
+        circleArray.length = 1;
+        setTimeout(init, 3000)
+    }
+    else {
+        console.log("Miss!");
+    }
 }
 
-
-function Circle(x, y, dx, dy, radius) {
+function Circle(x, y, color) {
     this.x = x;
     this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
-    this.minRadius = radius;
-    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+    this.dx = 0;
+    this.dy = 0;
+    this.radius = RADIUS;
+    this.color = color || colorArray[Math.floor(Math.random() * colorArray.length)];
 
     this.draw = function(){
         c.beginPath();
@@ -115,39 +65,34 @@ function Circle(x, y, dx, dy, radius) {
         }
         this.x += this.dx;
         this.y += this.dy;
-
-
-        // interactivity
-
-        if (mouse.x-this.x < DISTANCE_PX && mouse.x - this.x > -DISTANCE_PX && mouse.y-this.y < DISTANCE_PX && mouse.y - this.y > -DISTANCE_PX) {
-            if (this.radius < MAX_RADIUS){
-                this.radius += 1.5;
-            }
-
-        } else if (this.radius > this.minRadius){
-                this.radius -= 1;
-        }
-
+        
         this.draw();
     }
 }
 
-let circleArray = [];
-
 function init(){
     circleArray = [];
-    avoidArray = [[mouse.x,mouse.y]];
+    colorArray = [
+    "#3caea3",
+    "#f4c095",
+    "#ed553b",
+    "#f6d55c"
+    ]
 
-    for (let i = 0; i < 400; i++) {
-        let radius = Math.random() * 5 + 2;
-        let x = canvas.width/2;
-        let y = canvas.height/2;
-        let dx =(Math.random()-0.5) * 2;
-        let dy =(Math.random()-0.5) * 2;
-        circleArray.push(new Circle(x,y, dx, dy, radius));
-        
+    let randomColorArrayIndex = Math.floor(Math.random() * colorArray.length);
+    let chosenColor = colorArray.splice(randomColorArrayIndex, 1);
+
+    for (let i = 0; i < 200; i++) {
+        let x = Math.floor(Math.random()*(canvas.width - 2 * RADIUS + 1) + RADIUS);
+        let y = Math.floor(Math.random()*(canvas.height - 2 * RADIUS + 1) + RADIUS);
+        if (i == 0) {
+            circleArray.push(new Circle(x, y, chosenColor));
+        }
+        else {
+            circleArray.push(new Circle(x, y));
+
+        }
     }
-
 }
 
 function animate() {
@@ -156,9 +101,7 @@ function animate() {
 
     for (let i = 0; i < circleArray.length; i++) {
         circleArray[i].update();
-    }
-
-    
+    }    
 }
 
 init();
