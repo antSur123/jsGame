1b45934df8cac019f8bc73bcd632c9e229dda83a
antSur123 <113517280+antSur123@users.noreply.github.com>
2023-01-29 19:43:02 +0100

    Started Module 1 + tweaks

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Plannering https://docs.google.com/document/d/15m22qdkUSni4wRro1tTjpulOBNoxaa2P1uKWIwTHs8A/edit
-// Dokumentation https://docs.google.com/document/d/15m22qdkUSni4wRro1tTjpulOBNoxaa2P1uKWIwTHs8A/edit
+// Dokumentation https://docs.google.com/document/d/1Wij25DYcz6bglkBAEUzfXRDs5U3jSjaIIoH0hM12gHU/edit
 
-// Sets up canvases.
+// Sets up canvases and relvant tools.
 var gameMapCanvas = document.getElementById("game-map");
 gameMapCanvas.width = window.innerWidth * 0.9;
-gameMapCanvas.height = window.innerHeight - 1;
+gameMapCanvas.height = window.innerHeight;
 var gmc = gameMapCanvas.getContext('2d');
 
 var sidePanelCanvas = document.getElementById("side-panel");
@@ -16,63 +16,26 @@ var spc = sidePanelCanvas.getContext('2d');
 const RADIUS = 35;
 const WIDTH = 65;
 const HEIGHT = WIDTH;
-const BORDERWIDTH = +getComputedStyle(document.getElementById('side-panel')).borderRightWidth.slice(0, -2);
-const SIDEPANELOFFSET = BORDERWIDTH + sidePanelCanvas.width;
+const BORDER_WIDTH = +getComputedStyle(document.getElementById('side-panel')).borderRightWidth.slice(0, -2);
+const SIDEPANEL_OFFSET = BORDER_WIDTH + sidePanelCanvas.width;
+const START_TIME = 5;
+let timeLeft;
 let wantedCharacter;
-
-
-// Checks if player clicks.
-window.addEventListener('mousedown', (event) => {
-    if (characterArray.length > 1) {
-        clickDetection(event.x, event.y);
-    }
-});
-
-
-// Runs foundWantedCharacter() if clicked on the wanted char, and runs foundWrongCharacter() if not.
-function clickDetection(mouseX, mouseY) {
-    let charX = wantedCharacter.x + SIDEPANELOFFSET;
-    let charY = wantedCharacter.y;
-
-    console.log("char", charX, charY);
-    console.log("mouse", mouseX, mouseY);
-
-    if (wantedCharacter.type == "Square") {
-        let isWithinX = mouseX > charX - 1 && mouseX < charX + WIDTH + 1;
-        let isWithinY = mouseY > charY - 1 && mouseY < charY + HEIGHT + 1;
-        isWithinX && isWithinY ? foundWantedCharacter() : foundWrongCharacter();
-    }
-
-    else if (wantedCharacter.type == "Circle") {
-        let distance = Math.sqrt(Math.abs(Math.pow(mouseX - charX, 2) - Math.pow(mouseY - charY, 2)));
-        distance <= RADIUS ? foundWantedCharacter() : foundWrongCharacter();
-    }
-}
-
-
-// This wins the round and starts the next one after 3000 ticks.
-function foundWantedCharacter() {
-    console.log("Found!");
-    characterArray.length = 1;
-    setTimeout(startNextRound, 3000);
-}
-
-
-// This punishes the player for clicking on the wrong char.
-function foundWrongCharacter() {
-    console.log("Miss!");
-}
+let timer;
+let isWantedCharacterFound = false;
 
 
 // Constructor for Circles.
 class Circle {
-    constructor(x, y, color) {
+    constructor(x, y, canvas, color) {
         this.x = x;
         this.y = y;
         this.dx = 0;
         this.dy = 0;
         this.type = this.constructor.name;
         this.color = color || colorArray[Math.floor(Math.random() * colorArray.length)];
+        this.canvas = gmc || canvas;
+
 
         // Updates character's animation.
         this.update = () => {
@@ -85,11 +48,11 @@ class Circle {
 
             this.x += this.dx;
             this.y += this.dy;
-            this.draw();
+            this.draw(this.canvas);
         };
 
         // Draws character on the game canvas, or on the side panel canvas.  
-        this.draw = (canvas = gmc) => {
+        this.draw = (canvas) => {
             if (canvas != gmc) {
                 canvas.beginPath();
                 canvas.arc(sidePanelCanvas.width / 2, sidePanelCanvas.height / 2, RADIUS, 0, Math.PI * 2, false);
@@ -112,13 +75,14 @@ class Circle {
 
 // Constructor for Squares.
 class Square {
-    constructor(x, y, color) {
+    constructor(x, y, canvas, color) {
         this.x = x;
         this.y = y;
         this.dx = 0;
         this.dy = 0;
         this.type = this.constructor.name;
         this.color = color || colorArray[Math.floor(Math.random() * colorArray.length)];
+        this.canvas = gmc || canvas;
 
 
         // Updates character's animation.
@@ -132,16 +96,18 @@ class Square {
 
             this.x += this.dx;
             this.y += this.dy;
-            this.draw();
+            this.draw(this.canvas);
         };
 
 
         // Draws character on the game canvas, or on the side panel canvas.  
-        this.draw = (canvas = gmc) =>{
+        this.draw = (canvas) => {
             if (canvas != gmc) {
                 canvas.fillStyle = this.color;
-                canvas.fillRect((sidePanelCanvas.width - WIDTH) / 2, (sidePanelCanvas.height - HEIGHT) / 2, WIDTH, HEIGHT);
-                canvas.strokeRect((sidePanelCanvas.width - WIDTH) / 2, (sidePanelCanvas.height - HEIGHT) / 2, WIDTH, HEIGHT);
+                let x = (sidePanelCanvas.width - WIDTH) / 2;
+                let y = (sidePanelCanvas.height - HEIGHT) / 2;
+                canvas.fillRect(x, y, WIDTH, HEIGHT);
+                canvas.strokeRect(x, y, WIDTH, HEIGHT);
             }
             else {
                 canvas.fillStyle = this.color;
@@ -153,11 +119,112 @@ class Square {
 }
 
 
-// Starts new game.
-function startNextRound() {
-    spc.clearRect(0, 0, gameMapCanvas.width, gameMapCanvas.height);
+// Constructor for text.
+class Text {
+    constructor(y, text, fontSize) {
+        this.text = text;
+        this.fontSize = fontSize;
+        this.y = y;
 
-    characterArray = [];
+        // Updates text's info.
+        this.update = () => {
+            this.width = spc.measureText(this.text).width;
+            this.x = (sidePanelCanvas.width - this.width ) / 2;
+            this.draw();
+        };
+
+        // Draws text.
+        this.draw = () => {
+            spc.fillStyle = "red";
+            spc.font = `${this.fontSize}px Arial`;
+            spc.fillText(this.text, this.x, this.y);
+        };
+    }
+}
+
+
+// Checks if player clicks.
+window.addEventListener('mousedown', (event) => {
+    if (gameMapItems.length > 1) {
+        clickDetection(event.x, event.y);
+    }
+});
+
+
+// Runs foundWantedCharacter() if clicked on the wanted char, and runs foundWrongCharacter() if not.
+function clickDetection(mouseX, mouseY) {
+    let charX = wantedCharacter.x + SIDEPANEL_OFFSET;
+    let charY = wantedCharacter.y;
+
+    console.log("char", charX, charY);
+    console.log("mouse", mouseX, mouseY);
+
+    if (wantedCharacter.type == "Square") {
+        let isWithinX = mouseX > charX - 1 && mouseX < charX + WIDTH + 1;
+        let isWithinY = mouseY > charY - 1 && mouseY < charY + HEIGHT + 1;
+        isWithinX && isWithinY ? foundWantedCharacter() : foundWrongCharacter();
+    }
+
+    else if (wantedCharacter.type == "Circle") {
+        let distance = Math.sqrt(Math.abs(Math.pow(mouseX - charX, 2) - Math.pow(mouseY - charY, 2)));
+        distance <= RADIUS ? foundWantedCharacter() : foundWrongCharacter();
+    }
+}
+
+
+// This wins the round and starts the next one after 3000 ticks.
+function foundWantedCharacter() {
+    console.log("Found!");
+    isWantedCharacterFound = true;
+    gameMapItems.length = 1;
+    for (let i = 0 ; i < 2; i++) {
+        if (timeLeft <= 49) {
+            timeLeft += 1;
+        }
+    }
+    clearTimeout(countdown)
+    timer.text = `${timeLeft}s`;
+    setTimeout(startNextRound, 3000);
+}
+
+
+// This punishes the player for clicking on the wrong char.
+function foundWrongCharacter() {
+    console.log("Miss!");
+}
+
+
+// This counts down the timer. If it reaches 0 it ends the game.
+function countdown() {
+    console.log("timeLeft: " + timeLeft);
+    if (!isWantedCharacterFound) {
+	    timeLeft -= 1;
+	    timer.text = `${timeLeft}s`;
+	
+	    if (timeLeft == 0) {
+	        console.log("Time's up!");
+	        gameOver();
+	    }
+	    else if (timeLeft > 0) {
+	        setTimeout(countdown, 1000)
+	    }
+    }
+}
+
+
+// Ends game.
+function gameOver() {
+    console.log("Game Over!");
+    gameMapItems.length = 1;
+}
+
+
+// Starts new round.
+function startNextRound() {
+    isWantedCharacterFound = false;
+
+    gameMapItems = [];
+    sidePanelItems = []; 
     colorArray = [
         "#3caea3",
         "#f4c095",
@@ -172,9 +239,11 @@ function startNextRound() {
     generateCharacters(100);
 
     // Recolors the wanted character.
-    wantedCharacter = characterArray[0];
+    wantedCharacter = gameMapItems[0];
     wantedCharacter.color = chosenColor;
-    wantedCharacter.draw(spc);
+    
+    timer.draw();
+    setTimeout(countdown, 1000)
 }
 
 
@@ -182,16 +251,16 @@ function startNextRound() {
 function generateCharacters(ammount) {
     for (let i = 0; i < ammount; i++) {
         let x = Math.floor(Math.random() * (gameMapCanvas.width - 2 * WIDTH) + (WIDTH / 1.5));
-        let y = Math.floor(Math.random() * (gameMapCanvas.height - 2 * WIDTH) + (WIDTH / 1.5));
+        let y = Math.floor(Math.random() * (gameMapCanvas.height - 2 * HEIGHT) + (HEIGHT / 1.5));
         let randomShape = Math.floor(Math.random() * 2);
+
         if (randomShape == 0) {
-            characterArray.push(new Circle(x, y));
+            gameMapItems.push(new Circle(x, y, gmc));
         }
         else if (randomShape == 1) {
-            characterArray.push(new Square(x, y));
+            gameMapItems.push(new Square(x, y, gmc));
         }
     }
-
 }
 
 
@@ -199,11 +268,22 @@ function generateCharacters(ammount) {
 function animationFrame() {
     requestAnimationFrame(animationFrame);
     gmc.clearRect(0, 0, gameMapCanvas.width, gameMapCanvas.height);
+    spc.clearRect(0, 0, gameMapCanvas.width, gameMapCanvas.height);
+    
+    timer.update();
+    wantedCharacter.draw(spc);
 
-    for (let i = 0; i < characterArray.length; i++) {
-        characterArray[i].update();
+    for (let i = 0; i < gameMapItems.length; i++) {
+        gameMapItems[i].update();
     }
 }
 
-startNextRound();
-animationFrame();
+function gameInit() {
+    timeLeft = START_TIME;  // START_TIME
+    timer = new Text(100, `${timeLeft}s`, 60);
+
+    startNextRound();
+    animationFrame();
+}
+
+gameInit();
